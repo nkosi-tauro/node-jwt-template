53d6f43182cb96a60c856c2e705502fb2b0ca5be
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-19 15:53:10 +0200

    added jwt

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/register',async (req, res) => {
     const salt = await bcrypt.genSalt(10)
