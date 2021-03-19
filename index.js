b74ece5e4666e6b6e228dee808c41095b51e4b99
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-19 15:34:44 +0200

    updated mongoose connection driver

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-require('dotenv')
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/routes')
 
 app = express()
+app.use(express.json())
 app.use('/api',routes)
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@vuejwtauth.mpdvx.mongodb.net/jwtdb?retryWrites=true&w=majority`, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-}, (err) => {
-    console.log('connected to DB')
-})
+mongoose 
+ .connect(process.env.MONGO_PROD_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("Connected to Vue JWT Auth Databse"))
+ .catch(err => console.log(err));
 
 
 
