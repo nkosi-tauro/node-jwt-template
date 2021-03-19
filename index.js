af0af5f683b18c0dc1c881e688109814c38a59fb
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-19 14:13:51 +0200

    added mongo connection and dotenv

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require('dotenv')
 const express = require('express')
 const mongoose = require('mongoose')
 
-mongoose.connect()
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@vuejwtauth.mpdvx.mongodb.net/jwtdb?retryWrites=true&w=majority`, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}, (err) => {
+    console.log('connected to DB')
+})
 app = express()
 app.get('/', (req, res) => {
     res.send('Hello')
